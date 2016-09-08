428038cdb12b03812717ccadea481700406b3ff0
KeeganBruer <keegan.bruer@gmail.com>
2016-09-08 08:14:18 -0400

    My Work So Far. 9/8/2016
    FRC code template.

---
+++ src/Robot.cpp
@@ -0,0 +1,86 @@
+#include "WPILib.h"
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <iostream>
+
+
+
+
+class Robot: public IterativeRobot
+{
+
+
+	/*
+	 *  Declare all Objects (Robot Drive, Motors, Joysticks, Inputs)
+	 *
+	 *  Also Declare Different Variables (Ask John)
+	 *
+	 */
+
+
+public:
+	Robot() :
+		/*
+		 * Set All Objects to have a port on the roboRio.
+		 */
+
+		launchAngle(8)
+	{
+
+
+		 // Declare certian things when robot starts.
+
+
+		SmartDashboard::init();
+	}
+	~Robot()
+	{
+
+		// releasing memory that was allocated on the heap
+
+	}
+
+
+	void PublishDash()	//publishes all values to the smartdashboard.
+	{
+		//SmartDashboard::Put<Type (ie. number or boolean)>("Name Of Place On Dashboard" , Number);
+		// Example : SmartDashboard::PutNumber("Pot Voltage", pot.GetAverageVoltage());
+
+	}
+
+private:
+
+	void RobotInit()	//Code that executes on startup.
+	{
+
+	}
+
+	void AutonomousInit()	//Code that executes at the beginning of auto.
+	{
+
+	}
+
+	void AutonomousPeriodic()	//Autonomous code.
+	{
+
+	}
+
+	void TeleopInit()	//executes at the beginning of teleop.
+	{
+
+	}
+
+	void TeleopPeriodic()		//Code that runs during teleop.
+	{
+
+	}
+
+	void TestPeriodic()	//this executes if you switch to the test section of the dashboard.
+	{
+
+	}
+};
+
+START_ROBOT_CLASS(Robot)
